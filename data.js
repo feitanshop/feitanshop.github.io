81c8fc5c3065d23a81682989dc32ddddc701366b
feitanshop <vinsumokusanji22@gmail.com>
2026-04-08 01:32:38 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,16 @@
+const accounts = [
 
+  // GENSHIN
+  {id:1, game:"genshin", server:"eu", level:50, price:20},
+  {id:2, game:"genshin", server:"na", level:60, price:35},
+
+  // WUTHERING
+  {id:3, game:"wuthering", server:"sea", level:30, price:15},
+
+  // HONKAI
+  {id:4, game:"honkai", server:"asia", level:45, price:25},
+
+  // ZENLESS
+  {id:5, game:"zenless", server:"eu", level:28, price:18},
+
+];
